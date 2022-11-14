a513efacc70fe6f7910e6e50354198eedd12dfbd
dretekin <dretekin@gmail.com>
2022-11-14 03:25:25 -0500

    add script files processing

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,43 @@ const { src, dest, watch, series } = require("gulp");
 const nunjucksRender = require("gulp-nunjucks-render");
 
 function nunjucks(cb) {
-	src("src/index.html")
+	src("src/*.html")
 		.pipe(nunjucksRender({ path: "src/" }))
 		.pipe(dest("dist/"));
 	cb();
 }
 
 function css(cb) {
-	const styles = fs.readdirSync("src/styles");
-	let stylesRules = "";
-	styles.forEach((style) => {
-		stylesRules += fs.readFileSync(`src/styles/${style}`, {
-			encoding: "utf-8",
-		});
+	// const styles = fs.readdirSync("src/styles");
+	const styleSheets = ["base"];
+	let styleRules = "";
+	styleSheets.forEach((styleSheet) => {
+		const comment = `
+/*************************
+
+	${styleSheet}
+				
+**************************/
+`;
+		styleRules +=
+			comment +
+			fs.readFileSync(`src/styles/${styleSheet}.css`, {
+				encoding: "utf-8",
+			});
 	});
-	fs.writeFileSync("dist/style.css", stylesRules);
+	fs.writeFileSync("dist/style.css", styleRules);
+	cb();
+}
+
+function js(cb) {
+	src("src/scripts/*").pipe(dest("dist/"));
 	cb();
 }
 
 exports.default = function () {
 	// watch("src/*.html", nunjucks);
-	watch(["src/*.html", "src/styles/*"], series(css, nunjucks));
+	watch(
+		["src/*.html", "src/styles/*", "src/scripts/*"],
+		series(css, js, nunjucks)
+	);
 };
